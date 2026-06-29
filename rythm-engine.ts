02535ff40908f27341213afbbe5d412d3c55ea0b
geno0707 <geno0707@rocketmail.com>
2026-06-29 17:35:31 -0400

    Update rythm-engine.ts

---
+++ rythm-engine.ts
@@ -0,0 +1,13 @@
+
+
+const board: string[] = [" ", " ", " ", " "];
+const playableZone: string[] = ["←", "→", "↑", "↓"];  
+
+function makeBoard() {
+    //
+}
+
+function makePlayableZone() {
+    //
+}
+

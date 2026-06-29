374c14b0fe928e368444e30e2b7c953a38ca3ac5
geno0707 <geno0707@rocketmail.com>
2026-06-29 19:27:29 -0400

    Update rythm-engine.ts

--- rythm-engine.ts
+++ rythm-engine.ts
@@ -1,13 +1,41 @@
 
 
-const board: string[] = [" ", " ", " ", " "];
+
+const board: string[][] = [[" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "],
+    [" ", " ", " ", " "]];
+
 const playableZone: string[] = ["←", "→", "↑", "↓"];  
 
-function makeBoard() {
+
+function makeBoard(arr: string[][] = board, arr2: string[]) {
     //
+    console.log(arr);
+    console.log(arr2);
 }
 
 function makePlayableZone() {
     //
 }
 
+function readInput() {
+    //
+}
+
+function randomFall() {
+    //
+}
+
+function readSongSheet() {
+    //
+}
+
+class RythmEngine {
+    constructor() {
+        makeBoard(board, playableZone);
+    }
+}
+
+const rythmEngine = new RythmEngine();

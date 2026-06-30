9657ef9aed51f8511611d1a33a8e6c82a63a8c30
geno0707 <geno0707@rocketmail.com>
2026-06-30 13:04:03 -0400

    Update rythm-engine.ts

--- rythm-engine.ts
+++ rythm-engine.ts
@@ -1,5 +1,16 @@
+/*
+how the game works.
+on a board, a direction key will fall ( left, right, up, down) like tetris.
+the bottom part where it shows the arrows, are the playable area.
+your goal is to time it or hit it as soon as the falling direction falls to the playable area.
+
+scoring and combos.
+-
+-
+-
 
 
+*/
 
 const board: string[][] = [[" ", " ", " ", " "],
     [" ", " ", " ", " "],
@@ -11,7 +22,8 @@ const playableZone: string[] = ["←", "→", "↑", "↓"];
 
 
 function makeBoard(arr: string[][] = board, arr2: string[]) {
-    //
+    //this function will create the board and playable zone
+    //it 
     console.log(arr.map(row => row.map(c => `[${c}]`).join(" ")).join("\n"));
     console.log(arr2.map(c => `[${c}]`).join(" "));
 }
@@ -23,13 +35,29 @@ function makePlayableZone() {
 function readInput() {
     //
 }
-
-function randomFall() {
+function randomKeySpawn() {
     //
 }
 
 function readSongSheet() {
     //
+}
+function spawn(){
+    //this handles the spawning of the blocks
+}
+function fall(){
+    //this handles the falling of the blocks. 
+    //it takes: 
+    //in technical: 
+    //1.
+    //2.
+    //3.
+}
+function fallSpeed(){
+    //this handles the falling speed, as in the difficulty.
+}
+function setDifficulty(){
+
 }
 
 class RythmEngine {

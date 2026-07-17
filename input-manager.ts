8f4c27a8e8ee46c32bb04e0267bc3dd87ab1c53d
geno0707 <geno0707@rocketmail.com>
2026-07-17 03:29:31 -0400

    default hardmode music added.

--- input-manager.ts
+++ input-manager.ts
@@ -4,13 +4,25 @@
 import { readInput, isValidInput } from './rythm-engine.js';
 import * as readline from "readline";
 import sound from "sound-play";
+import { spawn } from "child_process";
 
+sound.play(("songs/hardmode.mp3"));
+const music = spawn("powershell", [
+  "-c",
+  `Add-Type -AssemblyName presentationCore; ` +
+  `$p = New-Object System.Windows.Media.MediaPlayer; ` +
+  `$p.Open([uri]"$PWD/songs/hardmode.mp3"); ` +
+  `$p.Play(); Start-Sleep -Seconds 600`
+]);
 
 readline.emitKeypressEvents(process.stdin);
 if (process.stdin.isTTY) process.stdin.setRawMode(true);
 
 process.stdin.on("keypress", (str, key) => {
-    if (key.ctrl && key.name === 'c') process.exit();
+    if (key.ctrl && key.name === 'c') {
+        music.kill();
+        process.exit();
+    }
 
     
     if (key.name === "up") readInput("^");
@@ -18,5 +30,3 @@ process.stdin.on("keypress", (str, key) => {
     if (key.name === "left") readInput("<");
     if (key.name === "right") readInput(">");
 })
-
-//sound.play("");
